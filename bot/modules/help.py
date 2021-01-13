7a384589e83cd943e7c4f887f47c7e0794a67a4a
Vivek-Kolhe <61515675+Vivek-Kolhe@users.noreply.github.com>
2021-01-13 17:24:45 +0530

    Initial Commits

--- bot/modules/help.py
+++ bot/modules/help.py
@@ -1,5 +1,6 @@
 from bot import EMILIA
 from pyrogram import filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 HELP_TEXT = """
 **Available Commands:**
@@ -9,6 +10,17 @@ HELP_TEXT = """
 - **/schedule <day>**
 """
 
+INFO_TEXT = """
+**Report issues:** @pookie_0_0
+"""
+
 @EMILIA.on_message(filters.command(["help"], prefixes = "/") & ~filters.edited)
 async def help(client, message):
     await EMILIA.send_message(chat_id = message.chat.id, text = HELP_TEXT, parse_mode = "markdown")
+
+@EMILIA.on_message(filters.command(["info"], prefixes = "/") & ~filters.edited)
+async def info(client, message):
+    buttons = [
+                [InlineKeyboardButton("Source", url = "https://github.com/Vivek-Kolhe/Emilia"), InlineKeyboardButton("Report", url = "https://t.me/pookie_0_0")]
+                ]
+    await EMILIA.send_message(chat_id = message.chat.id, text = INFO_TEXT, reply_markup = InlineKeyboardMarkup(buttons))
