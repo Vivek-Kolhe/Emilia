4fa54e34c91e78732f9ea165b912870dc70b4433
Vivek-Kolhe <61515675+Vivek-Kolhe@users.noreply.github.com>
2021-01-17 13:11:38 +0530

    __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,8 +1,10 @@
 from bot.config import Config
 from pyrogram import Client, filters
+from jikanpy import Jikan
 
 API_ID = Config.API_ID
 API_HASH = Config.API_HASH
 BOT_TOKEN = Config.BOT_TOKEN
 
+jikan = Jikan()
 EMILIA = Client("Emilia", api_id = API_ID, api_hash = API_HASH, bot_token = BOT_TOKEN)
