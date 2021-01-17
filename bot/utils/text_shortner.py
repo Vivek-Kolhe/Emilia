6040ff89895c4a48e70f4af1356fa0c467594a67
Vivek-Kolhe <61515675+Vivek-Kolhe@users.noreply.github.com>
2021-01-17 13:08:22 +0530

    Update text_shortner.py

--- bot/utils/text_shortner.py
+++ bot/utils/text_shortner.py
@@ -1,7 +1,8 @@
-def make_short(description, mal_url):
-    if len(description) < 650:
+def make_short(description, thumb, mal_url):
+    if len(description) < 750:
+        description += f"\n[R]({thumb})[ead more!]({mal_url})"
         return description
     else:
-        description = description[:651]
-        description += f"...\n[Read more!]({mal_url})"
+        description = description[:751]
+        description += f"[...]({thumb})\n[Read more!]({mal_url})"
         return description
