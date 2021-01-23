8f5674d941f20090ac9acaff2911f0b2ebdb3ee9
Vivek-Kolhe <61515675+Vivek-Kolhe@users.noreply.github.com>
2021-01-23 17:45:47 +0530

    changed /info to /about

--- bot/modules/help.py
+++ bot/modules/help.py
@@ -4,6 +4,7 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 HELP_TEXT = """
 **Available Commands:**
+- **/mal_id <unique_id>**
 - **/anime <anime_name>**
 - **/manga <manga_name>**
 - **/character <character_name>**
@@ -11,16 +12,16 @@ HELP_TEXT = """
 """
 
 INFO_TEXT = """
-**Report issues:** @pookie_0_0
+Made with ❤️ by @pookie_0_0
 """
 
 @EMILIA.on_message(filters.command(["help"], prefixes = "/") & ~filters.edited)
 async def help(client, message):
     await EMILIA.send_message(chat_id = message.chat.id, text = HELP_TEXT, parse_mode = "markdown")
 
-@EMILIA.on_message(filters.command(["info"], prefixes = "/") & ~filters.edited)
+@EMILIA.on_message(filters.command(["about"], prefixes = "/") & ~filters.edited)
 async def info(client, message):
     buttons = [
-                [InlineKeyboardButton("Source", url = "https://github.com/Vivek-Kolhe/Emilia"), InlineKeyboardButton("Report", url = "https://t.me/pookie_0_0")]
+                [InlineKeyboardButton("⭐ Me?", url = "https://github.com/Vivek-Kolhe/Emilia"), InlineKeyboardButton("Report Issues", url = "https://t.me/pookie_0_0")]
               ]
     await EMILIA.send_message(chat_id = message.chat.id, text = INFO_TEXT, reply_markup = InlineKeyboardMarkup(buttons))
